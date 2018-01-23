dea3c89700c445862f0b28cbe57137658f8d943a
Egor Kostan <igorkostan@gmail.com>
2018-01-23 00:11:38 -0800

    Update TestAPI.java

--- src/classes/TestAPI.java
+++ src/classes/TestAPI.java
@@ -3,7 +3,7 @@ package classes;
 
 public class TestAPI {
 
-	public final static String URL = "http://qatest-env1.us-west-2.elasticbeanstalk.com/api";
+	public final static String URL = "http://..../api";
 	public final static String USERS = "/users";
 	public final static String RESET = "/users/reset";
 
